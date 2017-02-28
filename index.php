f310f52125c2c7d62b11566b24dadef42848fa81
YOSHIE MIZOGUCHI <thisisamamail@gmail.com>
2017-03-01 07:47:13 +0900

    内容の修正

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <title>感想投稿フォーム</title>
 </head>
 <body>
-    <h1>お問い合わせフォーム</h1>
-    <p>名前と感想を入力してください</p>
-    <form action="inquiry.php" method="post">
-
+    <h1>感想投稿フォーム</h1>
+    <p>お名前とご感想を入力してください</p>
+    <form action="confirm.php" method="post">
         名前: <input type="text" name="name">
         感想: <input type="text" name="impression">
         <input type="submit" value="感想を投稿">
